51c629c5914335cd9e0d9c0c6ff67e4aad612957
afuetterer <fuetterh@posteo.de>
2019-03-06 11:41:32 +0100

    Remove legacy files and directories #16

--- lib_R/run_v31_de.R
+++
@@ -1,7 +0,0 @@
-library(r2ddi)
-
-dir2xml(
-    "temp/v31/de/",
-    "r2ddi/v31/de/",
-    missing_codes=-9:-1,
-    my_cores=30)
--- lib_R/run_v31_en.R
+++
@@ -1,7 +0,0 @@
-library(r2ddi)
-
-dir2xml(
-    "temp/v31/en/",
-    "r2ddi/v31/en/",
-    missing_codes=-9:-1,
-    my_cores=30)
